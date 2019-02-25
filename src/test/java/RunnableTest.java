aa5920713874fde5f5558240ab33b438bf3ad257
dongxuliu <18035879@suningcultural.com>
2019-02-25 18:27:52 +0800

    线程测试；

--- src/test/java/RunnableTest.java
+++ src/test/java/RunnableTest.java
@@ -11,29 +11,44 @@ public class RunnableTest {
         (new Runnable(){
             @Override
             public void run() {
-                synchronized (ob){
                     try {
-                        ob.wait();
-//                        ob.notify();
-                        System.out.println("");
+                        for(int i=0;i<1000000;i++){
+                            System.out.println("线程11111111111");
+                        }
+
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
-                    System.out.println("释放ob");
-                }
-                System.out.println("线程开始执行");
+//                    System.out.println("释放ob");
             }
+//                System.out.println("线程开始执行");
         }).run();
 
-//        (new Runnable(){
-//            @Override
-//            public void run() {
-//                synchronized (ob){
-//                    ob.notify();
-//                    System.out.println("notify 完成");
-//                }
-//            }
-//        }).run();
-    }
+        (new Runnable(){
+        @Override
+        public void run() {
+            for(int i=0;i<1000000;i++){
+                System.out.println("线程222222222222");
+            }
+        }
+    }).run();
+//        ThreadInstance thread1 = new ThreadInstance("线程11111111111111");
+//        Thread t1 = new Thread(thread1);
+//        ThreadInstance thread2 = new ThreadInstance("线程22222222222222");
+//        Thread t2 = new Thread(thread2);
+//        t1.start();t2.start();
+}
+public static class ThreadInstance implements Runnable{
+        String str;
 
+    public  ThreadInstance(String str){
+    this.str = str;
+    };
+    @Override
+    public void run() {
+        for(int i=0;i<10000000;i++){
+            System.out.println(str);
+        }
+    }
+}
 }
