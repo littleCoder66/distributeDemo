cfd49df8fcfc7333c8a23e077ea597510dafca9e
Your Name <you@example.com>
2019-11-28 22:34:46 +0800

    merge

--- src/test/java/CountDownLatchTest.java
+++ src/test/java/CountDownLatchTest.java
@@ -1,64 +1,7 @@
-<<<<<<< HEAD
 import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-
-public class CountDownLatchTest {
-    public static void main(String args[]){
-        CountDownLatch lock = new CountDownLatch(10);
-        ThreadPoolExecutor pool = new ThreadPoolExecutor(10, 15, 60,
-                TimeUnit.SECONDS,new BlockingDeque<Runnable>(10) worke)
-    }
-//    public static void main(String[] args) throws InterruptedException {
-//
-//        final CountDownLatch countDownLatch = new CountDownLatch(1) {
-//            @Override
-//            public void await() throws InterruptedException {
-//                super.await();
-//                System.out.println(Thread.currentThread().getName() + " count down is ok");
-//            }
-//        };
-//
-//        Thread thread1 = new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                //do something
-//                try {
-//                    Thread.sleep(1000);
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-//                System.out.println(Thread.currentThread().getName() + " is done");
-//                countDownLatch.countDown();
-//            }
-//        }, "thread1");
-//
-//        Thread thread2 = new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                //do something
-//                try {
-//                    Thread.sleep(2000);
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-//                System.out.println(Thread.currentThread().getName() + " is done");
-//                countDownLatch.countDown();
-//            }
-//        }, "thread2");
-//
-//
-//        thread1.start();
-//        thread2.start();
-//
-//        countDownLatch.await();
-//    }
-
-
-
-
-=======
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -117,5 +60,4 @@ public class CountDownLatchTest {
 //
 //        countDownLatch.await();
     }
->>>>>>> f8103bf836d6c6180103460d7fab40b66fe07f2f
 }
