c50233cd021003c000ff87b37753443c53615cc1
“liudongxu <18035879@suning.com>
2019-12-02 19:02:39 +0800

    spring 配置

---
+++ dubbo_consumer/src/main/java/cn/coder/consumer/ConsumerTest.java
@@ -0,0 +1,27 @@
+package cn.coder.consumer;
+
+import com.api.service.DemoService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+
+/**
+ * @author-liudongxu
+ * @created 2018-11-13 17:26
+ */
+public class ConsumerTest {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "classpath:springmvc.xml" });
+
+        context.start();
+        DemoService demoService = (DemoService) context.getBean("demoService");
+
+        System.out.println(demoService.sayHello(" worl!"));
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ dubbo_consumer/src/main/java/cn/coder/consumer/controller/TestController.java
@@ -0,0 +1,11 @@
+package cn.coder.consumer.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author-liudongxu
+ * @created 2019-12-02 18:17
+ */
+@Controller
+public class TestController {
+}
